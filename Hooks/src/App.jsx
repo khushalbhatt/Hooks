13a220276700cc8f37ca19cf9bf8d4313a0d3a69
Khushal Kumar <khushalbhatt17@gmail.com>
2025-02-21 23:08:05 +0530

    To fetch a particular post and render it

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [id, setId] = useState(0);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -17,14 +18,31 @@ function App() {
       });
   }, []);
 
+  const Fetchpost = (id) => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(response => {
+        setData([response.data]);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.log(error);
+        setLoading(false);
+      })
+  }
+
   if (loading) {
     return <h1>Loading...</h1>
   }
   return (
     <div>
+      <h1>To fetch a particular post</h1>
+      <input type="text" placeholder="Enter post id" onChange={e=>setId(e.target.value)}/>
+      <button onClick={()=>Fetchpost(id)}>Fetch Post</button>
+      <hr />
+
       <h1>Posts</h1>
         {data.map(post => (
-          <Posts key={post.id} title={post.title}></Posts>
+          <Posts key={post.id} title={post.title} body={post.body}></Posts>
         ))}
     </div>
   )
@@ -34,6 +52,7 @@ function Posts(props) {
   return (
     <div>
       <h2>{props.title}</h2>
+      <p>{props.body}</p>
     </div>
   )
 }
