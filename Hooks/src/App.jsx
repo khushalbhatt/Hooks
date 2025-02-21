a21fba81e0ace6231e7bf3921f7a68e3aa22e805
Khushal Kumar <khushalbhatt17@gmail.com>
2025-02-22 00:52:02 +0530

    useMemo Hook and useCallback

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 function App() {
-  const [id, setId] = useState(0);
+  const [id, setId] = useState();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -29,6 +29,22 @@ function App() {
         setLoading(false);
       })
   }
+  useMemo(()=>{
+    if(!id) return;
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then(response => {
+      setData([response.data]);
+      setLoading(false);
+    })
+    .catch(error => {
+      console.log(error);
+      setLoading(false);
+    })
+  },[id])
+
+  const fun = useCallback(() => {
+    console.log('Hello');
+  },[])
 
   if (loading) {
     return <h1>Loading...</h1>
@@ -36,13 +52,13 @@ function App() {
   return (
     <div>
       <h1>To fetch a particular post</h1>
-      <input type="text" placeholder="Enter post id" onChange={e=>setId(e.target.value)}/>
-      <button onClick={()=>Fetchpost(id)}>Fetch Post</button>
+      <input type="text" placeholder="Enter post id" onChange={(e)=>setId(e.target.value)}/>
+      <button onClick={()=>setId(id)}>Fetch Post</button>
       <hr />
 
       <h1>Posts</h1>
         {data.map(post => (
-          <Posts key={post.id} title={post.title} body={post.body}></Posts>
+          <Posts key={post.id} title={post.title} body={post.body} fun={fun}></Posts>
         ))}
     </div>
   )
